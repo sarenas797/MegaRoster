613fad0a71ac95d7cfb43636ed2501e5953cb972
Samuel Arenas <sarenas@purdue.edu>
2017-05-17 16:49:09 -0400

    update to recent

--- js/app.js
+++ js/app.js
@@ -1,43 +1,65 @@
-
 $(document).foundation()
 
 const megaroster = {
-    students: [],
+  students: [],
 
-    init(){
-        this.max=0 //counter for student id
-        this.studentList = document.querySelector('#student-list')
-        document
-            .querySelector('#new-student')
-            .addEventListener('submit', this.addStudent.bind(this)) //using the function, not the value returned from the func
-    },
+  init() {
+    this.studentList = document.querySelector('#student-list')
+    this.max = 0
+    this.setupEventListeners()
+  },
 
-    addStudent(ev) {
-        ev.preventDefault()
-        const f = ev.target
-        const student = {
-            id: this.max + 1,
-            name: f.studentName.value, //it targets the form and value of student name for the user input. Could replace ev.target with "this"
-        }
+  setupEventListeners() {
+    document
+      .querySelector('#new-student')
+      .addEventListener('submit', this.addStudent.bind(this))
+  },
 
-        this.students.unshift(student) //adds the new student values into the students array at the start of the array
+  addStudent(ev) {
+    ev.preventDefault()
+    const f = ev.target
+    const student = {
+      id: this.max + 1,
+      name: f.studentName.value,
+    }
 
-        const listItem = this.buildListItem(student) //even when inside an object, when called by a function "this" refers to the event not the object. adding bind will fix this
-        this.prependChild(this.studentList, listItem) // adds elements to the top of the list
-        this.max++
-        f.reset() //clears field or f.studentName.value = ""
-    },
+    this.students.unshift(student)
+    
+    const listItem = this.buildListItem(student)
+    this.prependChild(this.studentList, listItem)
+    
+    this.max ++
+    f.reset()
+  },
 
-    prependChild(parent,child){//function for adding child to the top
-        parent.insertBefore(child, parent.firstChild)
+  prependChild(parent, child) {
+    parent.insertBefore(child, parent.firstChild)
+  },
 
-    },
+  buildListItem(student) {
+    const template = document.querySelector('.student.template')
+    const li = template.cloneNode(true)
+    li.querySelector('.student-name').textContent = student.name
+    li.dataset.id = student.id
+    this.removeClassName(li, 'template')
 
-    buildListItem(student) {
-        const li = document.createElement('li')
-        li.textContent = student.name
-        li.dataset.id = student.id //sets the data id of the list element equal to the student id
-        return li
-    },
+    li
+      .querySelector('button.remove')
+      .addEventListener('click', this.removeStudent.bind(this))
+
+    return li
+  },
+
+  removeStudent(ev) {
+    const btn = ev.target
+    btn.closest('.student').remove()
+
+    // Remove it from the this.students array
+    // this.students.splice(?, 1)
+  },
+
+  removeClassName(el, className) {
+    el.className = el.className.replace(className, '').trim()
+  }
 }
 megaroster.init()
