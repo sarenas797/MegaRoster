ad95d792e9994b338fc1d295a72b864c273bd36e
Samuel Arenas <sarenas@purdue.edu>
2017-05-17 15:33:40 -0400

    add a data attribute to each list item

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ const megaroster = {
             name: f.studentName.value, //it targets the form and value of student name for the user input. Could replace ev.target with "this"
         }
 
-        this.students.push(student) //adds the new student values into the student array
+        this.students.push(student) //adds the new student values into the students array
 
         const listItem = this.buildListItem(student) //even when inside an object, when called by a function "this" refers to the event not the object. adding bind will fix this
         this.studentList.appendChild(listItem)
@@ -30,6 +30,7 @@ const megaroster = {
     buildListItem(student) {
         const li = document.createElement('li')
         li.textContent = student.name
+        li.dataset.id = student.id //sets the data id of the list element equal to the student id
         return li
     },
 }
