d0563d852e0a7a38c494e1f5cf0198eaaf965c24
Samuel Arenas <sarenas@purdue.edu>
2017-05-17 14:46:13 -0400

    setup submit handler for the form

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
+
 $(document).foundation()
+
+const megaroster={
+    init(){
+        document
+            .querySelector('#new-student')
+            .addEventListener('submit', this.addStudent) //using the function, not the value returned from the func
+    },
+
+    addStudent(ev){
+        ev.preventDefault()
+        const studentName = ev.target.studentName.value //it targets the form and value of student name for the user input could replace ev.target with "this"
+        console.log(studentName)
+    },
+}
+megaroster.init()
