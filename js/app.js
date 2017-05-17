d460b06b3f8191c657c9e5db9ba45972bc7f25cf
Samuel Arenas <sarenas@purdue.edu>
2017-05-17 15:10:42 -0400

    create student object with name and ID

--- js/app.js
+++ js/app.js
@@ -1,17 +1,29 @@
 
 $(document).foundation()
 
-const megaroster={
+const megaroster = {
     init(){
+        this.max=0 //counter for student id
         document
             .querySelector('#new-student')
-            .addEventListener('submit', this.addStudent) //using the function, not the value returned from the func
+            .addEventListener('submit', this.addStudent.bind(this)) //using the function, not the value returned from the func
     },
 
-    addStudent(ev){
+    addStudent(ev) {
         ev.preventDefault()
-        const studentName = ev.target.studentName.value //it targets the form and value of student name for the user input could replace ev.target with "this"
-        console.log(studentName)
+        const f = ev.target
+        const student = {
+            id: this.max + 1,
+            name: f.studentName.value, //it targets the form and value of student name for the user input. Could replace ev.target with "this"
+        }
+
+        this.buildListItem(student) //even when inside an object, when called by a function "this" refers to the event not the object. adding bind will fix this
+        this.max++
+    },
+
+    buildListItem(student) {
+        console.log(student)
+
     },
 }
 megaroster.init()
