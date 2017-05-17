deb3a849946b07e961d75d2ab98c771667862ff6
Samuel Arenas <sarenas@purdue.edu>
2017-05-17 15:47:14 -0400

    Add students to the top of the list

--- js/app.js
+++ js/app.js
@@ -20,12 +20,17 @@ const megaroster = {
             name: f.studentName.value, //it targets the form and value of student name for the user input. Could replace ev.target with "this"
         }
 
-        this.students.push(student) //adds the new student values into the students array
+        this.students.unshift(student) //adds the new student values into the students array at the start of the array
 
         const listItem = this.buildListItem(student) //even when inside an object, when called by a function "this" refers to the event not the object. adding bind will fix this
-        this.studentList.appendChild(listItem)
+        this.prependChild(this.studentList, listItem) // adds elements to the top of the list
         this.max++
         f.reset() //clears field or f.studentName.value = ""
+    },
+
+    prependChild(parent,child){//function for adding child to the top
+        parent.insertBefore(child, parent.firstChild)
+
     },
 
     buildListItem(student) {
