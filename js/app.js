60dffbee6d44da629b84a7e73581c1402198c6fa
Samuel Arenas <sarenas@purdue.edu>
2017-05-18 10:27:52 -0400

    finish homework 3

--- js/app.js
+++ js/app.js
@@ -7,6 +7,10 @@ const megaroster = {
     this.studentList = document.querySelector('#student-list')
     this.max = 0
     this.setupEventListeners()
+    localStorage.setItem('list', document.querySelector('.student').parentNode.childNodes)
+    if(performance.navigation.type===1){
+      var item = localStorage.getItem('list')
+    }
   },
 
   setupEventListeners() {
@@ -46,8 +50,48 @@ const megaroster = {
     li
       .querySelector('button.remove')
       .addEventListener('click', this.removeStudent.bind(this))
-
+    li.querySelector('button.success').addEventListener('click', this.promote.bind(this))
+    li.querySelector('button.secondary').addEventListener('click', this.moveUp.bind(this))
+    li.querySelector('button.movedown').addEventListener('click', this.moveDown.bind(this))
     return li
+    
+
+  },
+
+  moveDown(ev){
+    const btn = ev.target
+    const swap=btn.closest('.student').nextSibling
+    const orig=btn.closest('.student')
+    const parent = btn.closest('.student').parentNode
+    if(parent.lastChild.previousSibling===swap){
+      alert('This is the bottom of the list')
+    }
+    else{
+      parent.insertBefore(swap,orig)
+    }
+  },
+
+  moveUp(ev){
+    const btn = ev.target
+    const swap=btn.closest('.student').previousSibling
+    const orig=btn.closest('.student')
+    const parent = btn.closest('.student').parentNode
+    if(swap===null){
+      alert('This is the top of the list')
+    }
+    else{
+      parent.insertBefore(orig,swap)
+    }
+  },
+
+  promote(ev){
+    const btn = ev.target
+      if(btn.closest('.student').style.backgroundColor==='orange'){
+      btn.closest('.student').style.backgroundColor='white'
+    }
+    else{
+    btn.closest('.student').style.backgroundColor='orange'
+    }
   },
 
   removeStudent(ev) {
@@ -55,7 +99,7 @@ const megaroster = {
     btn.closest('.student').remove()
 
     // Remove it from the this.students array
-    // this.students.splice(?, 1)
+    this.students.splice(megaroster.length, 1)
   },
 
   removeClassName(el, className) {
